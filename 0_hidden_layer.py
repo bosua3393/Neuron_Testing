eab7f2fb6eb45cb9fbada8406262f6074b169408
yuichan2122 <33638496+yuichan2122@users.noreply.github.com>
2019-11-07 15:37:18 -0500

    Update and rename yui.py to 0_hidden_layer.py

--- 0_hidden_layer.py
+++ 0_hidden_layer.py
@@ -25,11 +25,11 @@ test_data = array([[1, 0, 1, 0, 1, 1],
 test_output = array([[1], [1], [0], [1], [0]])
 
 
-yui = Neuron(6, 1)
-yui.learn(train_data, train_output, 10000)
+model = Neuron(6, 1)
+model.learn(train_data, train_output, 10000)
 
-print(yui.weights, yui.biases)
+print(model.weights, model.biases)
 print("Solving .33...")
-print(round(yui.think(test_data)))
+print(round(model.think(test_data)))
 print("%error:")
-print(round((test_output-yui.think(test_data))*100))
+print(round((test_output-model.think(test_data))*100))
